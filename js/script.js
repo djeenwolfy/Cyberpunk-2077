fb73e4e53a374385dbb5f2a38d576edd560ae4ce
djeenwolfy <volkovpavelvladislavovich@gmail.com>
2018-10-15 17:48:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+   $(".main").click(function(){     
+      $("p").animate({top:220},1000);    
+      $("p").animate({left:320},1000);
+      
+   });
+ });
+
+// $(document).ready(function(){
+  
+//    $("p").mouseover(function(){$("p").css("color","yellow")});
+//    $("p").mouseout(function(){$("p").css("color","black")});
+
+// });
+// $("div").add("p").add("h1").css("background-color","#999")
